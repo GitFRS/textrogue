676b3fb6fe07f591bfdbc66d8cf438fc0b3bdce7
GitFRS <therocka321@gmail.com>
2021-01-20 17:39:27 +0300

    ver 0.0.2

--- game.py
+++ game.py
@@ -1,34 +1,54 @@
+import os
 
-def show_screen(screen):
-    for i in range(height + 1):
-        for j in range(width):
-            print(screen[i][j], end='')
-        print()
 
-height = 30
-width = 100
-screen = []
-for i in range(30):
-    for j in range(100):
-        if i == 0:
-             screen.append('-' * 100)
-             break
-        elif i == 1:
-             screen.append('|' + '1. Новая игра' + ' ' * 85 + '|')
-             break
-        elif i == 2:
-            screen.append('|' + '2. Продолжить игру' + ' ' * 80 + '|')
-            break
-        elif i == 3:
-            screen.append('|' + '3. Настройки' + ' ' * 86 + '|')
-            break
-        elif i == 4:
-            screen.append('|' + '4. Выход' + ' ' * 90 + '|')
-            break
-    if i not in (0, 1, 2, 3, 4, 99):
-        screen.append('|' + ' ' * 98 + '|')
-screen.append('-' * 100)
-show_screen(screen)
+def check_is_command():
+    global cmd_usr
+    global game_over
+    if cmd_usr == '/help':
+        for s in help_text:
+            print(s)
+    elif cmd_usr == '/new_game':
+        print("\nВы уверены, что хотите начать новую игру? (y/n)")
+        print("При начале новой игры весь ваш прогресс сбрасывается.\n")
+        cmd_usr = input('>')
+        if cmd_usr == 'y':
+            print("\n%ПОКА ЧТО НЕ РАБОТАЕТ%\n")
+        elif cmd_usr == 'n':
+            print("\n%ПОКА ЧТО НЕ РАБОТАЕТ%\n")  # А должно ли?
+    elif cmd_usr == '/settings':
+        print("\n%ПОКА ЧТО НЕ РАБОТАЕТ%\n")
+    elif cmd_usr == '/clear':                    # Разные аргументы в system в
+        os.system("clear")                       # зависимости от платформы
+    elif cmd_usr == '/save':
+        print("\n%ПОКА ЧТО НЕ РАБОТАЕТ%\n")
+    elif cmd_usr == '/exit':
+        input("\nНажмите любую клавишу для выхода из игры...")
+        game_over = True
 
-#TODO: Необходимо сделать ввод команд пользователем после строки "4. Выход".
-#      Чтобы указать пользователю, что можно что-то вводить, можно использовать '>>>'
+
+def play():
+    global game_over
+    global main_text
+    global cmd_usr
+    cmd_usr = input('>')
+
+
+cmd_usr = ''
+game_over = False
+main_text = ['%GAME NAME%\n\n\n', 'Для ввода команд напечатайте их\n',
+             'на клавиатуре и нажмите Enter.\n',
+             'Чтобы начать новую игру, введите /new_game\n']
+help_text = ['\nСписок команд:\n\n',
+             '/help    список команд\n',
+             '/new_game    начать новую игру\n',
+             '/settings    настройки\n',
+             '/clear    очистить экран\n',
+             '/save    сохранить игру\n',
+             '/exit    выйти из игры\n']
+
+for s in main_text:
+    print(s)
+
+while not game_over:
+    play()
+    check_is_command()
